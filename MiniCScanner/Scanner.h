5d96a15c3fba42adee4421f295e784bfc31b76d0
Jaeyong Park <72537190+jaypyon@users.noreply.github.com>
2021-05-15 01:29:00 +0900

    Scanner.h 확장 구현
    Scanner.h 확장 구현

--- MiniCScanner/Scanner.h
+++ MiniCScanner/Scanner.h
@@ -11,42 +11,53 @@
 
 #include <stdio.h>
 
-#define NO_KEYWORD 7
+#define NO_KEYWORD 16
 #define ID_LENGTH 12
-
+#define CMT_LENGTH 1000
+#define MAX_LENGTH 80
 
 
 struct tokenType {
-    int number;
-    union {
-        char id[ID_LENGTH];
-        int num;
-    } value;
+    int token_number;
+    char token_value[MAX_LENGTH];
+    int column_number;
+    int line_number;
+    int type_of_comment;
+    //1: documented comments 2: multi line comment 
+    //3:single line documented comments 4: single line comment
+    char comment_content[CMT_LENGTH];
 };
 
 enum tsymbol {
     tnull = -1,
-    tnot, tnotequ, tremainder, tremAssign, tident, tnumber,
+    tnot,        tnotequ,      tremainder,tremAssign, tident,    tnumber,
     /* 0          1            2         3            4          5     */
-    tand, tlparen, trparen, tmul, tmulAssign, tplus,
+    tand,         tlparen,     trparen,  tmul,       tmulAssign, tplus,
     /* 6          7            8         9           10         11     */
-    tinc, taddAssign, tcomma, tminus, tdec, tsubAssign,
+    tinc,         taddAssign,  tcomma,  tminus,      tdec,      tsubAssign,
     /* 12         13          14        15           16         17     */
-    tdiv, tdivAssign, tsemicolon, tless, tlesse, tassign,
+    tdiv,         tdivAssign, tsemicolon,tless,      tlesse,    tassign,
     /* 18         19          20        21           22         23     */
-    tequal, tgreat, tgreate, tlbracket, trbracket, teof,
+    tequal,       tgreat,     tgreate,  tlbracket,   trbracket, teof,
     /* 24         25          26        27           28         29     */
+    
     //   ...........    word symbols ................................. //
+    
     /* 30         31          32        33           34         35     */
-    tconst, telse, tif, tint, treturn, tvoid,
-    /* 36         37          38        39                             */
-    twhile, tlbrace, tor, trbrace
+    tconst,       telse,      tif,      tint,        treturn,   tvoid,
+    /* 36         37          38        39           40         41     */
+    twhile,     tlbrace,      tor,      trbrace,     tchar,     tdouble,
+    /* 42         43          44        45           46         47     */
+    tfor,         tdo,        tgoto,    tswitch,     tcase,     tbreak,
+    /* 48		  49		  50		51			 52  		53	   */
+    tdefault,     tstringltr, tcharltr ,tcolon,      tcomment, treal
+   
 };
 
 
 int superLetter(char ch);
 int superLetterOrDigit(char ch);
-int getNumber(FILE *sourceFile, char firstCharacter);
+double getNumber(FILE *sourceFile, char firstCharacter);
 int hexValue(char ch);
 void lexicalError(int n);
 struct tokenType scanner(FILE *sourceFile);
